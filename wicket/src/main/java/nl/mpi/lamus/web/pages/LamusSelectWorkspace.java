62f58976dec723cbecb8a3d676d85e54891b13e6
Twan Goosen <t.goosen@gmail.com>
2012-07-17 11:46:26 +0000

    License info & settings

--- wicket/src/main/java/nl/mpi/lamus/web/pages/LamusSelectWorkspace.java
+++ wicket/src/main/java/nl/mpi/lamus/web/pages/LamusSelectWorkspace.java
@@ -1,23 +1,30 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2012 Max Planck Institute for Psycholinguistics
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package nl.mpi.lamus.web.pages;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Date;
-import nl.mpi.lamus.web.model.mock.MockWorkspace;
 import nl.mpi.lamus.workspace.factory.WorkspaceFactory;
 import nl.mpi.lamus.workspace.model.Workspace;
-import nl.mpi.lamus.workspace.model.WorkspaceStatus;
-import nl.mpi.lamus.workspace.model.implementation.LamusWorkspace;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.model.Model;
 
 /**
  *
