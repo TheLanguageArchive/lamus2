b263e24a4e4bc9143cc8e7a641a7dc1ed3ce37dc
Guilherme Silva <guilherme.silva@gmail.com>
2014-01-13 19:07:31 +0000

    The unlinked nodes table (a table-tree) was improved in order to show the links between the files. Also, only the top nodes of the table (the ones without any parent) can be selected using a checkbox.
    refs #3426

--- wicket/src/main/java/nl/mpi/lamus/web/components/UnlinkedNodesPanel.java
+++ wicket/src/main/java/nl/mpi/lamus/web/components/UnlinkedNodesPanel.java
@@ -19,9 +19,12 @@ package nl.mpi.lamus.web.components;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import nl.mpi.lamus.service.WorkspaceService;
 import nl.mpi.lamus.web.pages.LamusPage;
+import nl.mpi.lamus.web.unlinkednodes.model.WorkspaceTreeNodeExpansion;
 import nl.mpi.lamus.web.unlinkednodes.providers.UnlinkedNodesModelProvider;
 import nl.mpi.lamus.web.unlinkednodes.tree.CheckedFolderContent;
 import nl.mpi.lamus.web.unlinkednodes.tree.Content;
@@ -34,12 +37,17 @@ import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColu
 import org.apache.wicket.extensions.markup.html.repeater.tree.AbstractTree;
 import org.apache.wicket.extensions.markup.html.repeater.tree.DefaultTableTree;
 import org.apache.wicket.extensions.markup.html.repeater.tree.TableTree;
+import org.apache.wicket.extensions.markup.html.repeater.tree.content.CheckedFolder;
+import org.apache.wicket.extensions.markup.html.repeater.tree.content.Folder;
+import org.apache.wicket.extensions.markup.html.repeater.tree.table.NodeModel;
 import org.apache.wicket.extensions.markup.html.repeater.tree.table.TreeColumn;
+import org.apache.wicket.extensions.markup.html.repeater.util.ProviderSubset;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.CheckGroup;
 import org.apache.wicket.markup.html.panel.GenericPanel;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.OddEvenItem;
+import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.resource.PackageResourceReference;
@@ -58,9 +66,12 @@ public class UnlinkedNodesPanel extends GenericPanel<Workspace> {
     
     private AbstractTree<WorkspaceTreeNode> unlinkedNodesTree;
     
-    private CheckGroup<WorkspaceTreeNode> checkgroup;
+//    private CheckGroup<WorkspaceTreeNode> checkgroup;
     
-    private Content content;
+//    private ProviderSubset<WorkspaceTreeNode> checked;
+    private Collection<WorkspaceTreeNode> checked;
+    
+//    private CheckedFolderContent content;
     
     
     public UnlinkedNodesPanel(String id, IModel<Workspace> model, UnlinkedNodesModelProvider provider) {
@@ -70,46 +81,97 @@ public class UnlinkedNodesPanel extends GenericPanel<Workspace> {
         
         add(new Label("dir", uploadFolder.getAbsolutePath()));
         
-        content = new CheckedFolderContent(provider);
+//        checked = new ProviderSubset<WorkspaceTreeNode>(provider, true);
+        checked = new ArrayList<WorkspaceTreeNode>();
         
-        checkgroup = new CheckGroup<WorkspaceTreeNode>("checkgroup", new ArrayList<WorkspaceTreeNode>()) {
-
-            @Override
-            protected boolean wantOnSelectionChangedNotifications() {
-                return true;
-            }
-
-            @Override
-            protected void onSelectionChanged(Collection<? extends WorkspaceTreeNode> newSelection) {
-                super.onSelectionChanged(newSelection); //To change body of generated methods, choose Tools | Templates.
-            }
-            
-            @Override
-            protected void onModelChanged() {
-                super.onModelChanged(); //To change body of generated methods, choose Tools | Templates.
-            }
-        };
+//        content = new CheckedFolderContent(provider);
         
-        unlinkedNodesTree = createTree("unlinkedNodesTableTree", provider);
+//        checkgroup = new CheckGroup<WorkspaceTreeNode>("checkgroup", new ArrayList<WorkspaceTreeNode>()) {
+//
+//            @Override
+//            protected boolean wantOnSelectionChangedNotifications() {
+//                return true;
+//            }
+//
+//            @Override
+//            protected void onSelectionChanged(Collection<? extends WorkspaceTreeNode> newSelection) {
+//                super.onSelectionChanged(newSelection); //To change body of generated methods, choose Tools | Templates.
+//                
+//                
+//                
+//                
+//                //TODO listener?
+//                    // and trigger visibility changes in the page (buttons, etc)
+//            }
+//            
+//            @Override
+//            protected void onModelChanged() {
+//                super.onModelChanged(); //To change body of generated methods, choose Tools | Templates.
+//            }
+//        };
+        
+        unlinkedNodesTree = createTree("unlinkedNodesTableTree", provider, new WorkspaceTreeNodeExpansionModel());
         unlinkedNodesTree.setOutputMarkupPlaceholderTag(true);
         
-        checkgroup.setOutputMarkupId(true);
+//        checkgroup.setOutputMarkupId(true);
+//        
+//        checkgroup.add(unlinkedNodesTree);
+//        
+//        add(checkgroup);
         
-        checkgroup.add(unlinkedNodesTree);
-        
-        add(checkgroup);
+        add(unlinkedNodesTree);
     }
     
     
-    protected AbstractTree<WorkspaceTreeNode> createTree(String id, UnlinkedNodesModelProvider provider/*, IModel<Set<WorkspaceTreeNode>> state*/)
+    protected AbstractTree<WorkspaceTreeNode> createTree(String id, UnlinkedNodesModelProvider provider, IModel<Set<WorkspaceTreeNode>> state)
     {
         List<IColumn<WorkspaceTreeNode, String>> columns = createColumns();
 
-        final TableTree<WorkspaceTreeNode, String> tree = new DefaultTableTree<WorkspaceTreeNode, String>(id, columns, provider, Integer.MAX_VALUE/*, state*/) {
+        
+        
+        //TODO CREATE A SUBCLASS OF TABLE TREE WITH A METHOD THAT RETURNS THE SELECTED NODES FROM WITHIN THE NODES???
+        
+        
+        final TableTree<WorkspaceTreeNode, String> tree = new DefaultTableTree<WorkspaceTreeNode, String>(id, columns, provider, Integer.MAX_VALUE, state) {
 
             @Override
             protected Component newContentComponent(String id, IModel<WorkspaceTreeNode> model) {
-                return content.newContentComponent(id, unlinkedNodesTree, model);
+//                return content.newContentComponent(id, unlinkedNodesTree, model);
+                
+                
+                if(model.getObject().getParent() != null) {
+                    
+                    return new Folder<WorkspaceTreeNode>(id, unlinkedNodesTree, model);
+                    
+                } else {
+                    
+                    return new CheckedFolder<WorkspaceTreeNode>(id, unlinkedNodesTree, model) {
+                    
+                        private static final long serialVersionUID = 1L;
+                        
+                        @Override
+                        protected IModel<Boolean> newCheckBoxModel(final IModel<WorkspaceTreeNode> model) {
+                            
+                            return new IModel<Boolean>() {
+                                private static final long serialVersionUID = 1L;
+
+                                @Override
+                                public Boolean getObject() {
+                                    return isChecked(model.getObject());
+                                }
+
+                                @Override
+                                public void setObject(Boolean object) {
+                                    check(model.getObject(), object);
+                                }
+
+                                @Override
+                                public void detach() {
+                                }
+                            };
+                        }
+                    };
+                }
             }
 
             @Override
@@ -128,14 +190,16 @@ public class UnlinkedNodesPanel extends GenericPanel<Workspace> {
 
         columns.add(new PropertyColumn<WorkspaceTreeNode, String>(Model.of("ID"), "workspaceNodeID"));
         
-        columns.add(new TreeColumn<WorkspaceTreeNode, String>(Model.of("")) {
+        columns.add(new TreeColumn<WorkspaceTreeNode, String>(Model.of("Tree")));// {
 
-            @Override
-            public void populateItem(Item<ICellPopulator<WorkspaceTreeNode>> cellItem, String componentId, IModel<WorkspaceTreeNode> rowModel) {
-                
-                cellItem.add(new CheckBoxPanel(componentId, rowModel));
-            }
-        });
+//            @Override
+//            public void populateItem(Item<ICellPopulator<WorkspaceTreeNode>> cellItem, String componentId, IModel<WorkspaceTreeNode> rowModel) {
+//                
+////                cellItem.add(new CheckBoxPanel(componentId, rowModel));
+//                NodeModel<WorkspaceTreeNode> nodeModel = (NodeModel<WorkspaceNode>) rowModel;
+//                cellItem.add(new Label(componentId, nodeModel)
+//            }
+//        });
         
         columns.add(new PropertyColumn<WorkspaceTreeNode, String>(Model.of("Name"), "name"));
         columns.add(new PropertyColumn<WorkspaceTreeNode, String>(Model.of("Type"), "type"));
@@ -146,6 +210,46 @@ public class UnlinkedNodesPanel extends GenericPanel<Workspace> {
     
     public Collection<WorkspaceTreeNode> getSelectedUnlinkedNodes() {
         
-        return this.checkgroup.getModelObject();
+//        return this.checkgroup.getModelObject();
+//        return null;
+
+        return checked;
+    }
+    
+    
+    
+    
+    
+    
+    private Iterator<WorkspaceTreeNode> getChecked() {
+        return checked.iterator();
+    }
+    
+    private boolean isChecked(WorkspaceTreeNode wsTreeNode) {
+        return checked.contains(wsTreeNode);
+    }
+
+    private void check(WorkspaceTreeNode wsTreeNode, boolean check) {
+        if (check && !checked.contains(wsTreeNode)) {
+            checked.add(wsTreeNode);
+        }
+        else {
+            checked.remove(wsTreeNode);
+        }
+    }
+    
+    
+    
+    
+
+    private static class WorkspaceTreeNodeExpansionModel extends AbstractReadOnlyModel<Set<WorkspaceTreeNode>> {
+
+        public WorkspaceTreeNodeExpansionModel() {
+        }
+
+        @Override
+        public Set<WorkspaceTreeNode> getObject() {
+            return WorkspaceTreeNodeExpansion.get();
+        }
     }
 }
